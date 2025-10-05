2c2d88faaafc2c82322ba897cbce178500d6643a
Alex-490 <xionglion@163.com>
2025-10-05 12:40:55 -0700

    实验一第二题

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,7 +5,9 @@
 #define WR 1
 
 int main(int argv,char const* argc[]){
-    char buf='P';
+    char buf1[4]="ping";
+    char buf2[4]="pong";
+    char buf3[4]="pppp";
     
     int fd_c2p[2];
     int fd_p2c[2];
@@ -28,14 +30,14 @@ int main(int argv,char const* argc[]){
         close(fd_c2p[RD]);
         close(fd_p2c[WR]);
 
-        if(read(fd_p2c[RD],&buf,sizeof(char))!=sizeof(char)){
+        if(read(fd_p2c[RD],&buf3,sizeof(buf3))!=sizeof(buf3)){
             fprintf(2,"child read() error!\n");
             exit_status=1;
         }else{
-            fprintf(1,"%d: receive ping\n",getpid());
+            fprintf(1,"%d: receive %s\n",getpid(),buf3);
         }
 
-        if(write(fd_c2p[WR],&buf,sizeof(char))!=sizeof(char)){
+        if(write(fd_c2p[WR],&buf2,sizeof(buf2))!=sizeof(buf2)){
             fprintf(2,"child write() error!\n");
             exit_status=1;
         }
@@ -48,16 +50,16 @@ int main(int argv,char const* argc[]){
         close(fd_c2p[WR]);
         close(fd_p2c[RD]);
 
-        if(write(fd_p2c[WR],&buf,sizeof(char))!=sizeof(char)){
+        if(write(fd_p2c[WR],&buf1,sizeof(buf1))!=sizeof(buf1)){
             fprintf(2,"parent write() error!\n");
             exit_status=1;
         }
 
-        if(read(fd_c2p[RD],&buf,sizeof(char))!=sizeof(char)){
+        if(read(fd_c2p[RD],&buf1,sizeof(buf1))!=sizeof(buf1)){
             fprintf(2,"parent read() error!\n");
             exit_status=1;
         }else{
-            fprintf(1,"%d: receive pong\n",getpid());
+            fprintf(1,"%d: receive %s\n",getpid(),buf1);
         }
 
         close(fd_c2p[RD]);
