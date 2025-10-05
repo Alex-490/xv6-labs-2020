5f01b08a54bddee539e8549191b5618619f8e4d2
Alex-490 <xionglion@163.com>
2025-10-05 12:30:44 -0700

    实验一第二题

---
+++ user/pingpong.c
@@ -0,0 +1,69 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+#define RD 0
+#define WR 1
+
+int main(int argv,char const* argc[]){
+    char buf='P';
+    
+    int fd_c2p[2];
+    int fd_p2c[2];
+
+    pipe(fd_c2p);
+    pipe(fd_p2c);
+
+    int pid=fork();
+    int exit_status=0;
+
+    if(pid<0){
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+
+        fprintf(2,"fork() error!\n");
+        exit(1);
+    }else if(pid==0){
+        close(fd_c2p[RD]);
+        close(fd_p2c[WR]);
+
+        if(read(fd_p2c[RD],&buf,sizeof(char))!=sizeof(char)){
+            fprintf(2,"child read() error!\n");
+            exit_status=1;
+        }else{
+            fprintf(1,"%d: receive ping\n",getpid());
+        }
+
+        if(write(fd_c2p[WR],&buf,sizeof(char))!=sizeof(char)){
+            fprintf(2,"child write() error!\n");
+            exit_status=1;
+        }
+
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        
+        exit(exit_status);
+    }else{
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+
+        if(write(fd_p2c[WR],&buf,sizeof(char))!=sizeof(char)){
+            fprintf(2,"parent write() error!\n");
+            exit_status=1;
+        }
+
+        if(read(fd_c2p[RD],&buf,sizeof(char))!=sizeof(char)){
+            fprintf(2,"parent read() error!\n");
+            exit_status=1;
+        }else{
+            fprintf(1,"%d: receive pong\n",getpid());
+        }
+
+        close(fd_c2p[RD]);
+        close(fd_p2c[WR]);
+        
+        exit(exit_status);
+    }
+}
+
