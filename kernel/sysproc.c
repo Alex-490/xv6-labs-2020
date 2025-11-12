25de1ac0f0507e955dcd09cb6aae582cd2ef7bb1
Alex-490 <xionglion@163.com>
2025-11-12 15:43:19 -0800

    exercise 3 unfinished

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,8 +46,20 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  myproc()->sz+=n;
+
+  struct proc* p=myproc();
+  addr=p->sz;
+  uint64 sz=p->sz;
+  
+  if(n>0){
+    myproc()->sz+=n;
+  }else if(sz+n>0){
+    sz=uvmdealloc(p->pagetable,sz,sz+n);
+    p->sz=sz;
+  }else{
+    return -1;
+  }
+  
   return addr;
 }
 
